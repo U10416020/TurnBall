269e88e25dd449af6afff36cb04b82dc4ed6b2f1
U10416020 <wkps19961015@gmail.com>
2016-05-22 21:35:38 +0800

    Change the balls.
    It can exchange the ball and clear the balls.

--- TurnBall.java
+++ TurnBall.java
@@ -1,60 +1,99 @@
 package TurnBall;
 
-import java.security.SecureRandom;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import static javafx.application.Application.launch;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Eva
- */
 public class TurnBall extends Application {
     //剪貼簿    
     ClipboardContent content = new ClipboardContent();
     ImageView first = new ImageView();    
-    int count = 0;
-    double changeX,changeY, exChangeX, exChangeY;
     
+    int[][] number = new int[13][7];
+    int[][] record = new int[13][7];
+    ImageView[][] ball = new ImageView[6][5];
+    double changeX,changeY, exChangeX, exChangeY;
+    int sizeX = 510, sizeY=10,fit=97;
+    //CheckBall checkBall;
+    AllPane pane;
+    CheckBall checkBall;
+    DownBall downBall;
     @Override
     public void start(Stage primaryStage) {
-        AllPane pane = new AllPane();
-        ImageView[][] ball = new ImageView[5][6];
-        SecureRandom random = new SecureRandom();
+        pane = new AllPane();
+        pane.setStyle("-fx-background-color: black;");
+        FirstBall firstBall = new FirstBall();        
+                
+        number = firstBall.getNumber();        
         
-        for(int i = 0;i<5;i++){
-            for(int j = 0;j<6;j++){
-                int number = random.nextInt(6);
+        for(int i = 0;i<6;i++){
+            for(int j = 0;j<5;j++){                
                 ball[i][j] = new ImageView();
-                ball[i][j].setImage(pane.picture[number]);
-                ball[i][j].setFitHeight(80);
-                ball[i][j].setFitWidth(80);
-                ball[i][j].setX(500+i*80);
-                ball[i][j].setY(0+j*80);                
+                ball[i][j].setImage(pane.picture[number[i+6][j+1]]);
+                ball[i][j].setFitHeight(fit);
+                ball[i][j].setFitWidth(fit);
+                ball[i][j].setX(sizeX+j*fit);
+                ball[i][j].setY(sizeY+i*fit);                
                 ball[i][j].addEventHandler(MouseEvent.DRAG_DETECTED,onDragDetectedEventHandler);
                 ball[i][j].addEventHandler(DragEvent.DRAG_DONE,onDragDoneEventHandler);
                 ball[i][j].addEventHandler(DragEvent.DRAG_OVER,onDragOverEventHandler);
                 pane.getChildren().add(ball[i][j]);
+                System.out.print(number[i+6][j+1]+" ");
             }
+            System.out.println();
         }
         
-        Scene scene = new Scene(pane, 1000, 600);
+        Scene scene = new Scene(pane, 1030, 600);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hello World!");        
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    /*
+    void vertical(){
+        int countV = 0,countH=0;
+        for(int j = 1; j<6;j++){
+            for(int i = 1;i<7;i++){
+                V(i,j);
+                H(i,j);                
+            }
+        }
+        change();
+    }
+    void change(){
+        for(int i = 0;i<8;i++){
+            for(int j = 0;j<7;j++){
+                if(record[i][j]==1){
+                    number[i][j] = 6;
+                }
+            }
+        }
+    }
     
+    void V(int i ,int j){
+        if(number[i][j]==number[i+1][j]&&number[i][j]==number[i-1][j]){
+            record[i][j]=1;
+            record[i-1][j]=1;
+            record[i+1][j]=1;
+        }
+    }
+    
+    void H(int i ,int j){
+        if(number[i][j]==number[i][j+1]&&number[i][j]==number[i][j-1]){
+            record[i][j]=1;
+            record[i][j-1]=1;
+            record[i][j+1]=1;
+        }
+    }
+    */
     //偵測拖曳事件(來源)
     EventHandler onDragDetectedEventHandler = new EventHandler<MouseEvent>() {
         @Override
@@ -80,6 +119,22 @@ public class TurnBall extends Application {
         public void handle(DragEvent event) {
             System.out.println("DragDone");
             
+            //vertical();
+            
+            checkBall = new CheckBall(number);
+            number = checkBall.getNumber();
+            //
+            
+            for(int i = 0;i<6;i++){
+                for(int j = 0;j<5;j++){   
+                    ball[i][j].setX(sizeX+j*fit);
+                    ball[i][j].setY(sizeY+i*fit);  
+                    ball[i][j].setImage(pane.picture[number[i+6][j+1]]);
+                }
+            }
+            
+            downBall = new DownBall(number,ball);
+            //checkBall = new CheckBall(number);
             //if(event.getTransferMode()==TransferMode.MOVE){
                 ImageView source = (ImageView)event.getSource();
                 //source.setImage(content1.getImage());
@@ -98,7 +153,7 @@ public class TurnBall extends Application {
                 System.out.println("!!!!");
                 event.acceptTransferModes(TransferMode.ANY);
                 ImageView source = (ImageView)event.getSource();
-                System.out.println("SourceX: " + source.getX()+"\tSourceY: " + source.getY());
+                
                 changeX=first.getX();
                 changeY=first.getY();
                 exChangeX=source.getX();
@@ -110,17 +165,41 @@ public class TurnBall extends Application {
                 first.setX(exChangeX);
                 first.setY(exChangeY);
                 
-                System.out.println("FirstX: " + first.getX() + "\nFirstY: " + first.getY());                
+                int i = 6+(int)(exChangeY-sizeY)/fit;
+                int j = (int)(exChangeX-sizeX)/fit;
+                
+                for(int k = 0; k<6;k++){
+                    if(first.getImage() == pane.picture[k]){
+                        number[i][j+1] = k;
+                    }
+                }
+                
+                System.out.println("exChange I: " + i +" J: " + j+1 + " number: " + number[i+1][j+1]);
+                
+                i = 6+(int)(changeY-sizeY)/fit;
+                j = (int)(changeX-sizeX)/fit;                
+                
+                for(int k = 0; k<6;k++){
+                    if(source.getImage() == pane.picture[k]){
+                        number[i][j+1] = k;
+                    }
+                }
+                
+                System.out.println("Change I: " + i +" J: " + j+1 + " number: " +number[i][j+1]);
+                
+                for( i = 6;i<12;i++){
+            for( j = 1;j<6;j++){
+                System.out.print(number[i][j]+" ");
+            }
+            System.out.println();
+        }
                 
             }
             event.consume();            
         }
     };
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
-    }
-    
+    }    
 }
