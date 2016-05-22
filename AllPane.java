405bdc5cc98df15e0b7d5cbb36130e6a2366a93c
U10416020 <wkps19961015@gmail.com>
2016-05-22 21:29:56 +0800

    Add pictures
    Add up, down, bottom,and black back ground.

--- AllPane.java
+++ AllPane.java
@@ -6,19 +6,34 @@ import javafx.scene.layout.Pane;
 
 public class AllPane extends Pane{
 
-    Image[] picture = new Image[6];
+    Image[] picture = new Image[7];
     AllPane(){        
-        picture[0] = new Image("/image/red.png", 100, 100, false, false);
-        picture[1] = new Image("/image/yellow.png", 100, 100, false, false);
-        picture[2] = new Image("/image/blue.png", 100, 100, false, false);
-        picture[3] = new Image("/image/green.png", 100, 100, false, false);
-        picture[4] = new Image("/image/purple.png", 100, 100, false, false);
-        picture[5] = new Image("/image/pink.png", 100, 100, false, false);
+        picture[0] = new Image("/image/red.png", 97, 97, false, false);
+        picture[1] = new Image("/image/yellow.png", 97, 97, false, false);
+        picture[2] = new Image("/image/blue.png", 97, 97, false, false);
+        picture[3] = new Image("/image/green.png", 97, 97, false, false);
+        picture[4] = new Image("/image/purple.png", 97, 97, false, false);
+        picture[5] = new Image("/image/pink.png", 97, 97, false, false);
+        picture[6] = new Image("/image/empty.png", 97, 97, false, false);       
         
-        ImageView asdf = new ImageView(new Image("/image/紅史.png"));
-        asdf.setX(0);
-        asdf.setY(0); 
+        ImageView buttom = new ImageView(new Image("/image/buttom.png",535,610,false,false));
+        ImageView up = new ImageView(new Image("/image/up.png"));
+        ImageView down = new ImageView(new Image("/image/down.png"));
         
-        getChildren().add(asdf);
+        buttom.setX(485);
+        buttom.setY(0);
+        
+        up.setX(0);
+        up.setY(0);
+        up.setFitHeight(600);
+        up.setFitWidth(100);
+        
+        down.setX(965);
+        down.setY(0);
+        down.setFitHeight(600);
+        down.setFitWidth(100);
+        
+        getChildren().addAll(buttom,up,down);
+        
     }
 }
